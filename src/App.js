9fe95a43d24973efe915f7649c88f73a85fadc95
Hossein Sekhavat <sekhavat17@gmail.com>
2016-10-05 00:00:09 +0330

    Install mui

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, {Component} from 'react';
 import './App.css';
 import {VictoryChart, VictoryArea, VictoryGroup, VictoryLine} from 'victory';
 import InputRange from 'react-input-range';
-import _ from 'underscore';
 import 'react-input-range/dist/react-input-range.css';
+import 'muicss/dist/css/mui.min.css';
+import Appbar from 'muicss/lib/react/appbar';
+import Button from 'muicss/lib/react/button';
+import Container from 'muicss/lib/react/container';
+import Col from 'muicss/lib/react/col';
+import Row from 'muicss/lib/react/row';
 
 let memoize = new Map();
 
@@ -62,10 +67,10 @@ class MyChart extends Component {
     let p2 = integrate({from: hFrom, to: hTo, pdf: pdf2});
     return (
       <div>
-        <div style={{width: 500, textAlign: 'center', bottom: -20, position: 'relative'}}>
+        <div style={{textAlign: 'center', bottom: -20, position: 'relative'}}>
           Red: {p1.toFixed(5)} {""} Blue: {p2.toFixed(5)}
         </div>
-        <div style={{width: 500}}>
+        <div>
           <VictoryChart
             events={[{
               onClick: (x)=> {
@@ -87,7 +92,7 @@ class MyChart extends Component {
             </VictoryGroup>
           </VictoryChart>
         </div>
-        <div style={{width: 390, paddingLeft: 55}}>
+        <div style={{paddingLeft: 60, paddingRight: 60}}>
           <InputRange
             maxValue={to}
             minValue={from}
@@ -105,10 +110,20 @@ class App extends Component {
   render() {
     return (
       <div >
-        <MyChart from={10} to={50} pdf1={(x)=>10 / x} pdf2={(x)=>Math.sin(x) * Math.sin(x)}/>
-        <MyChart from={10} to={50} pdf1={(x)=>10 / x} pdf2={(x)=>Math.sin(x) * Math.sin(x)}/>
-        <MyChart from={10} to={50} pdf1={(x)=>10 / x} pdf2={(x)=>Math.sin(x) * Math.sin(x)}/>
-        <MyChart from={10} to={50} pdf1={(x)=>10 / x} pdf2={(x)=>Math.sin(x) * Math.sin(x)}/>
+        <Appbar></Appbar>
+        <Container>
+          <Row>
+            <Col xs="12" md="6" md-offset="3">
+              <Button color="primary">
+                <button></button>
+              </Button>
+              <MyChart from={10} to={50} pdf1={(x)=>10 / x} pdf2={(x)=>Math.sin(x) * Math.sin(x)}/>
+              <MyChart from={10} to={50} pdf1={(x)=>10 / x} pdf2={(x)=>Math.sin(x) * Math.sin(x)}/>
+              <MyChart from={10} to={50} pdf1={(x)=>10 / x} pdf2={(x)=>Math.sin(x) * Math.sin(x)}/>
+              <MyChart from={10} to={50} pdf1={(x)=>10 / x} pdf2={(x)=>Math.sin(x) * Math.sin(x)}/>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
